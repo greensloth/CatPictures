cc7d88a25a928c0efcab732058d323da1d4d5204
greensloth <triplecreepstack@gmail.com>
2025-10-06 13:56:24 +0500

    Создано окно, метка

--- cats.py
+++ cats.py
@@ -1,1 +1,22 @@
 import tkinter as tk
+from PIL import Image, ImageTk
+import requests
+from io import BytesIO
+
+def load_image
+
+window = tk.Tk()
+window.title("Cats")
+window.geometry("600x480")
+
+label = tk.Label()
+label.pack()
+
+url = 'https://cataas.com/cat'
+img = load_image(url)
+
+if img is not None:
+    label.configure(image=img)
+    label.image = img
+
+window.mainloop()
